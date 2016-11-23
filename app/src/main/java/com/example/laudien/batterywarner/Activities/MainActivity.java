18fe7361712f4a375b8aa17c71432ecce96f9fbd
pixelfehler1996 <checkercraft1996@gmail.com>
2016-11-23 13:29:22 +0100

    changed to toolbar

--- app/src/main/java/com/example/laudien/batterywarner/Activities/MainActivity.java
+++ app/src/main/java/com/example/laudien/batterywarner/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
         final SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         boolean firstStart = sharedPreferences.getBoolean(PREF_FIRST_START, true);
--- app/src/main/java/com/example/laudien/batterywarner/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/laudien/batterywarner/Activities/SettingsActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +38,10 @@ public class SettingsActivity extends AppCompatActivity implements CompoundButto
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.settings));
+        setSupportActionBar(toolbar);
+
         sharedPreferences = getSharedPreferences(Contract.SHARED_PREFS, MODE_PRIVATE);
 
         textView_lowBattery = (TextView) findViewById(R.id.textView_lowBattery);
