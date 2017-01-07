b02d0714cfa75f3b0150d3ef772a57f681bfdcbe
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-01-07 10:49:27 +0100

    1.51-beta release + added notification if silent mode is on

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/ChargingReceiver.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/ChargingReceiver.java
@@ -1,13 +1,21 @@
 package com.laudien.p1xelfehler.batterywarner.Receivers;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.support.v4.app.NotificationManagerCompat;
+import android.widget.Toast;
 
+import com.laudien.p1xelfehler.batterywarner.Activities.MainActivity;
 import com.laudien.p1xelfehler.batterywarner.Contract;
 import com.laudien.p1xelfehler.batterywarner.Database.GraphChargeDbHelper;
+import com.laudien.p1xelfehler.batterywarner.Fragments.SettingsFragment;
+import com.laudien.p1xelfehler.batterywarner.R;
 
 import java.util.Calendar;
 
@@ -47,6 +55,29 @@ public class ChargingReceiver extends BroadcastReceiver {
             sharedPreferences.edit().putLong(Contract.PREF_GRAPH_TIME, Calendar.getInstance().getTimeInMillis())
                     .putInt(Contract.PREF_LAST_PERCENTAGE, -1)
                     .apply();
+        }
+
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        int ringerMode = audioManager.getRingerMode();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        if (
+                !notificationManager.areNotificationsEnabled()
+                || ringerMode == AudioManager.RINGER_MODE_SILENT
+                || ringerMode == AudioManager.RINGER_MODE_VIBRATE)
+        {
+            PendingIntent contentIntent = PendingIntent.getActivity(
+                    context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+            Notification.Builder builder = new Notification.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSound(SettingsFragment.getNotificationSound(context))
+                    .setVibrate(new long[]{0, 300, 300, 300})
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setContentTitle(context.getString(R.string.app_name))
+                    .setContentText(context.getString(R.string.notifications_are_off))
+                    .setContentIntent(contentIntent)
+                    .setAutoCancel(true);
+            notificationManager.notify(0, builder.build());
         }
 
         // start new alarm
