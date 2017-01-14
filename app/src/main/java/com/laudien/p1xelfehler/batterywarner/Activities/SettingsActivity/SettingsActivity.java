ea06bfabc7b53f828a7e7e1d6eb804143642754b
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-01-14 23:15:07 +0100

    fixed theme not changing on back pressed

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/SettingsActivity.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/SettingsActivity.java
@@ -44,4 +44,9 @@ public class SettingsActivity extends BaseActivity {
         }
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public void onBackPressed() {
+        onNavigateUp();
+    }
 }
