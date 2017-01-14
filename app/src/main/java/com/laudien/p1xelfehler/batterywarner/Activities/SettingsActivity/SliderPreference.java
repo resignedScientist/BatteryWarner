bd0f77f36f35c2d82c4b75fec36ab75a91ff0e9a
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-01-14 15:40:38 +0100

    slider are working now! (but with 2 keys!)

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/SliderPreference.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/SliderPreference.java
@@ -4,26 +4,39 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.laudien.p1xelfehler.batterywarner.R;
 
-public class SliderPreference extends Preference implements CompoundButton.OnCheckedChangeListener {
+import java.util.Locale;
+
+public class SliderPreference extends Preference implements CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
 
     private static final String TAG = "SliderPreference";
     private static final int DEFAULT_MAX = 100;
+    private static final int DEFAULT_MIN = 0;
     private static final boolean DEFAULT_CHECKED = false;
+    private static final int DEFAULT_PROGRESS = 0;
+    private String key_progress = "";
     private int max;
+    private int min;
+    private String unit = "";
     private boolean checked;
     private boolean defaultChecked;
     private boolean initialized = false;
+    private int progress;
+    private int defaultProgress;
     private CheckBox checkBox;
     private SeekBar seekBar;
+    private TextView textView;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public SliderPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -50,12 +63,24 @@ public class SliderPreference extends Preference implements CompoundButton.OnChe
         setLayoutResource(R.layout.preference_slider);
         if (attrs != null) {
             TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SliderPreference);
+            unit = a.getString(R.styleable.SliderPreference_unit);
+            if (unit == null) {
+                unit = "";
+            }
+            key_progress = a.getString(R.styleable.SliderPreference_key_progress);
+            if (key_progress == null) {
+                key_progress = "";
+            }
             max = a.getInt(R.styleable.SliderPreference_slider_max, DEFAULT_MAX);
+            min = a.getInt(R.styleable.SliderPreference_slider_min, DEFAULT_MIN);
             defaultChecked = a.getBoolean(R.styleable.SliderPreference_default_checked, DEFAULT_CHECKED);
+            defaultProgress = a.getInt(R.styleable.SliderPreference_default_progress, DEFAULT_PROGRESS);
             a.recycle();
         } else {
+            min = DEFAULT_MIN;
             max = DEFAULT_MAX;
             defaultChecked = DEFAULT_CHECKED;
+            defaultProgress = DEFAULT_PROGRESS;
         }
     }
 
@@ -64,6 +89,10 @@ public class SliderPreference extends Preference implements CompoundButton.OnChe
         super.onSetInitialValue(restorePersistedValue, defaultValue);
         if (restorePersistedValue) { // load saved values
             checked = getPersistedBoolean(defaultChecked);
+            if (!key_progress.equals("")) { // load progress
+                progress = PreferenceManager.getDefaultSharedPreferences(getContext())
+                        .getInt(key_progress, defaultProgress);
+            }
             initialized = true;
         }
     }
@@ -73,14 +102,20 @@ public class SliderPreference extends Preference implements CompoundButton.OnChe
         super.onBindView(view);
         checkBox = (CheckBox) view.findViewById(R.id.checkBox);
         checkBox.setOnCheckedChangeListener(this);
+        textView = (TextView) view.findViewById(R.id.textView);
         seekBar = (SeekBar) view.findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+        seekBar.setMax(max - min);
+
+        Log.i(TAG, "progress = " + progress);
 
         if (initialized) {
             checkBox.setChecked(checked);
+            seekBar.setProgress(progress - min);
         } else {
             checkBox.setChecked(defaultChecked);
+            seekBar.setProgress(defaultProgress - min);
         }
-        seekBar.setMax(max);
     }
 
     @Override
@@ -95,4 +130,23 @@ public class SliderPreference extends Preference implements CompoundButton.OnChe
         persistBoolean(b);
     }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        textView.setText(String.format(Locale.getDefault(), "%d%s", i + min, unit));
+        if (!key_progress.equals("")) { // save the progress
+            PreferenceManager.getDefaultSharedPreferences(getContext()).edit()
+                    .putInt(key_progress, i + min)
+                    .apply();
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
