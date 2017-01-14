05c509deeaf6eddae7cb3ff57d9c396adfda8410
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-01-14 16:27:35 +0100

    restart service/alarm on settings close

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/NewSettingsFragment.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/NewSettingsFragment.java
@@ -1,14 +1,34 @@
 package com.laudien.p1xelfehler.batterywarner.Activities.SettingsActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
+import com.laudien.p1xelfehler.batterywarner.BatteryAlarmManager;
 import com.laudien.p1xelfehler.batterywarner.R;
+import com.laudien.p1xelfehler.batterywarner.Services.ChargingService;
 
 public class NewSettingsFragment extends PreferenceFragment {
+
+    // private static final String TAG = "NewSettingsFragment";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // restart discharging alarm and charging service
+        Context context = getActivity();
+        if (context != null) {
+            BatteryAlarmManager batteryAlarmManager = BatteryAlarmManager.getInstance(context);
+            batteryAlarmManager.cancelDischargingAlarm(context);
+            batteryAlarmManager.setDischargingAlarm(context);
+            context.startService(new Intent(context, ChargingService.class));
+        }
     }
 }
