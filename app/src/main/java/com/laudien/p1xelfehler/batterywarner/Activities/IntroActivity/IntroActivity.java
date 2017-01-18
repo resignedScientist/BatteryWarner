54a1b9a0feadcfacf71132ab4c8a4e9e12ff23bb
pixelfehler1996 <checkercraft1996@gmail.com>
2017-01-18 17:15:02 +0100

    fixed not going back to fullscreen if any dialog was shown in the intro

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/IntroActivity/IntroActivity.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/IntroActivity/IntroActivity.java
@@ -30,6 +30,16 @@ public class IntroActivity extends MaterialIntroActivity {
         //enableLastSlideAlphaExitTransition(true); // enable that nice transition at the end
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
+        // enable fullscreen if fullscreen was disabled (e.g. if a dialog opens)
+        View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener
+                (new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        enableFullscreen();
+                    }
+                });
+
         addSlide(new ImageSlide());
 
         addSlide(new SlideFragmentBuilder()
@@ -57,13 +67,7 @@ public class IntroActivity extends MaterialIntroActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        // enable full screen:
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        enableFullscreen();
     }
 
     @Override
@@ -90,5 +94,14 @@ public class IntroActivity extends MaterialIntroActivity {
         }
         Toast.makeText(getApplicationContext(), getString(R.string.intro_finish_toast), Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private void enableFullscreen() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
