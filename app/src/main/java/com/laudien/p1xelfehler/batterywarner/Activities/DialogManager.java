29419bf6437aa8fc1e560334847398b3d37e6590
pixelfehler1996 <checkercraft1996@gmail.com>
2017-01-19 08:50:21 +0100

    make code better readable

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/DialogManager.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/DialogManager.java
@@ -28,18 +28,33 @@ public class DialogManager {
         LayoutInflater inflater = activity.getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_graph_info, null);
         TextView textView_totalTime = (TextView) view.findViewById(R.id.textView_totalTime);
-        textView_totalTime.setText(String.format(Locale.getDefault(), "%s: %s",
-                activity.getString(R.string.charging_time), infoObject.getTimeString(activity)));
+        textView_totalTime.setText(String.format(
+                Locale.getDefault(),
+                "%s: %s",
+                activity.getString(R.string.charging_time),
+                infoObject.getTimeString(activity))
+        );
         TextView textView_maxTemp = (TextView) view.findViewById(R.id.textView_maxTemp);
-        textView_maxTemp.setText(String.format(Locale.getDefault(), "%s: %.1f°C",
-                activity.getString(R.string.max_temp), infoObject.getMaxTemp()));
+        textView_maxTemp.setText(String.format(
+                Locale.getDefault(),
+                "%s: %.1f°C",
+                activity.getString(R.string.max_temp),
+                infoObject.getMaxTemp())
+        );
         TextView textView_minTemp = (TextView) view.findViewById(R.id.textView_minTemp);
-        textView_minTemp.setText(String.format(Locale.getDefault(), "%s: %.1f°C",
-                activity.getString(R.string.min_temp), infoObject.getMinTemp()));
+        textView_minTemp.setText(String.format(
+                Locale.getDefault(),
+                "%s: %.1f°C",
+                activity.getString(R.string.min_temp),
+                infoObject.getMinTemp())
+        );
         TextView textView_speed = (TextView) view.findViewById(R.id.textView_speed);
-        textView_speed.setText(String.format(Locale.getDefault(), "%s: %.2f%%/h",
+        textView_speed.setText(String.format(
+                Locale.getDefault(),
+                "%s: %.2f%%/h",
                 activity.getString(R.string.charging_speed),
-                infoObject.getPercentCharged() * 60 / infoObject.getTimeInMinutes()));
+                infoObject.getPercentCharged() * 60 / infoObject.getTimeInMinutes())
+        );
         new AlertDialog.Builder(activity)
                 .setTitle(activity.getString(R.string.graph_info))
                 .setView(view)
