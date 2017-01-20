c5220b688ad5ca440dea8592445772bc5157466e
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-01-20 21:16:54 +0100

    replaced button in toolbar with preference

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/SettingsFragment.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/SettingsFragment.java
@@ -18,9 +18,6 @@ import android.preference.RingtonePreference;
 import android.preference.SwitchPreference;
 import android.preference.TwoStatePreference;
 import android.support.v4.content.ContextCompat;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.laudien.p1xelfehler.batterywarner.BatteryAlarmManager;
@@ -63,7 +60,6 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
         }
 
         if (!Contract.IS_PRO) {
-            setHasOptionsMenu(true);
             Preference pref_graphEnabled = findPreference(getString(R.string.pref_graph_enabled));
             pref_graphEnabled.setEnabled(false);
             Preference pref_timeFormat = findPreference(getString(R.string.pref_time_format));
@@ -72,25 +68,21 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
             PreferenceCategory category_graph = (PreferenceCategory) findPreference("stats");
             category_graph.setTitle(String.format(Locale.getDefault(),
                     "%s (%s)", getString(R.string.stats), getString(R.string.pro_only_short)));
+            Preference pref_pro = new Preference(context);
+            pref_pro.setTitle(getString(R.string.get_pro));
+            pref_pro.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    try {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + Contract.PACKAGE_NAME_PRO)));
+                    } catch (android.content.ActivityNotFoundException e) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + Contract.PACKAGE_NAME_PRO)));
+                    }
+                    return true;
+                }
+            });
+            category_graph.addPreference(pref_pro);
         }
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.settings_menu, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_pro) {
-            try {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + Contract.PACKAGE_NAME_PRO)));
-            } catch (android.content.ActivityNotFoundException anfe) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + Contract.PACKAGE_NAME_PRO)));
-            }
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
