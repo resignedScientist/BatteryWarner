e095edc7a61da3603d4fe23cbe1982cce2161a56
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-02-15 09:51:41 +0100

    removed firebase

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/MainActivity/MainActivity.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/MainActivity/MainActivity.java
@@ -9,12 +9,9 @@ import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.laudien.p1xelfehler.batterywarner.Activities.BaseActivity;
 import com.laudien.p1xelfehler.batterywarner.Activities.SettingsActivity.SettingsActivity;
 import com.laudien.p1xelfehler.batterywarner.Contract;
@@ -72,12 +69,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private void isAppInstalled() {
         // checks if 2 versions (free + pro) are installed and tells you that you have to uninstall the free one
-        Log.i(TAG, "Tag: " + FirebaseInstanceId.getInstance().getId());
-        Log.i(TAG, "Token: " + FirebaseInstanceId.getInstance().getToken());
-        FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
-        if (config.getBoolean("dual_app_alex_supi_dupi_mode")) {
-            return;
-        }
         PackageManager packageManager = getPackageManager();
         try {
             packageManager.getPackageInfo(Contract.PACKAGE_NAME_FREE, PackageManager.GET_ACTIVITIES);
