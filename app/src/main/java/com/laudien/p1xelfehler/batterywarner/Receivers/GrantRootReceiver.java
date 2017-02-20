3d585b714785d9716089f3445209a9d612fdb7db
pixelfehler1996 <checkercraft1996@gmail.com>
2017-02-20 15:13:21 +0100

    show toast if root was denied

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/GrantRootReceiver.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/GrantRootReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import com.laudien.p1xelfehler.batterywarner.R;
 import com.laudien.p1xelfehler.batterywarner.RootChecker;
@@ -12,15 +13,24 @@ import com.laudien.p1xelfehler.batterywarner.RootChecker;
 public class GrantRootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
-        AsyncTask.execute(new Runnable() {
+        new AsyncTask<Void, Void, Boolean>() {
+
             @Override
-            public void run() {
-                if (RootChecker.isDeviceRooted()) {
+            protected Boolean doInBackground(Void... voids) {
+                return RootChecker.isDeviceRooted();
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+                super.onPostExecute(aBoolean);
+                if (aBoolean) {
                     PreferenceManager.getDefaultSharedPreferences(context).edit()
                             .putBoolean(context.getString(R.string.pref_stop_charging), true).apply();
+                } else {
+                    Toast.makeText(context, context.getString(R.string.toast_root_denied),
+                            Toast.LENGTH_LONG).show();
                 }
             }
-        });
-
+        }.execute();
     }
 }
