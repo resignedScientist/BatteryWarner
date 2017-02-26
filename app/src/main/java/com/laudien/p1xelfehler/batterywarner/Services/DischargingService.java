516ee53b81f085424c055679ff5b5e2769aa6b55
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-02-26 11:39:01 +0100

    added preference strings for new service

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Services/DischargingService.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Services/DischargingService.java
@@ -5,11 +5,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
-public class DischargingService extends Service {
+import com.laudien.p1xelfehler.batterywarner.R;
 
+public class DischargingService extends Service implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences sharedPreferences;
     private BroadcastReceiver screenOnReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -25,8 +30,15 @@ public class DischargingService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        registerReceiver(screenOnReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
-        registerReceiver(screenOffReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        boolean serviceEnabled = sharedPreferences.getBoolean(getString(R.string.pref_discharging_service_enabled), getResources().getBoolean(R.bool.pref_discharging_service_enabled_default));
+        if (serviceEnabled) {
+            registerReceiver(screenOnReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
+            registerReceiver(screenOffReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+        } else {
+            stopSelf();
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -35,6 +47,14 @@ public class DischargingService extends Service {
         super.onDestroy();
         unregisterReceiver(screenOnReceiver);
         unregisterReceiver(screenOffReceiver);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals(getString(R.string.pref_discharging_service_enabled))
+                && !sharedPreferences.getBoolean(s, getResources().getBoolean(R.bool.pref_discharging_service_enabled_default))) {
+            stopSelf();
+        }
     }
 
     @Nullable
