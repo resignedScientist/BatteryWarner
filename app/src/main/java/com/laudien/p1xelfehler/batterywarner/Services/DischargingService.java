a8148df9cc8bd7cec8d2c07afc710c79e5b19d03
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-02-26 11:30:10 +0100

    register/unregister receiver

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Services/DischargingService.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Services/DischargingService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
@@ -22,19 +23,23 @@ public class DischargingService extends Service {
         }
     };
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        registerReceiver(screenOnReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
+        registerReceiver(screenOffReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        unregisterReceiver(screenOnReceiver);
+        unregisterReceiver(screenOffReceiver);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
