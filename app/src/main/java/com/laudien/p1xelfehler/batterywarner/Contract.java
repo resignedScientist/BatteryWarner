fa7d8ef7cf6625b36ee168b9aab861a1d865c9f3
pixelfehler1996 <checkercraft1996@gmail.com>
2017-03-01 16:31:19 +0100

    removed broadcast and its variable

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Contract.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Contract.java
@@ -7,7 +7,6 @@ public final class Contract {
     public static final String DATABASE_HISTORY_PATH = Environment.getExternalStorageDirectory() + "/BatteryWarner";
     // custom broadcasts
     public static final String BROADCAST_ON_OFF_CHANGED = "com.laudien.p1xelfehler.batterywarner.BROADCAST_ON_OFF_CHANGED";
-    public static final String BROADCAST_DB_CHANGED = "com.laudien.p1xelfehler.batterywarner.BROADCAST_DB_CHANGED";
     public static final String BROADCAST_BOTH_APPS_INSTALLED = "com.laudien.p1xelfehler.batterywarner.BOTH_APPS_INSTALLED";
     public static final String BROADCAST_DISCHARGING_ALARM = "com.laudien.p1xelfehler.batterywarner.DISCHARGING_ALARM";
     // package names
