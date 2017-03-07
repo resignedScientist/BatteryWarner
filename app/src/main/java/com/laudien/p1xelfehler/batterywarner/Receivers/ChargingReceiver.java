5294bd23989eb53ee1017d162df513bb9c6942c5
pixelfehler1996 <checkercraft1996@gmail.com>
2017-03-07 15:46:32 +0100

    check for root when starting to charge

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/ChargingReceiver.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/ChargingReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.BatteryManager;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -13,7 +14,10 @@ import android.support.v4.app.NotificationManagerCompat;
 import com.laudien.p1xelfehler.batterywarner.Contract;
 import com.laudien.p1xelfehler.batterywarner.NotificationBuilder;
 import com.laudien.p1xelfehler.batterywarner.R;
+import com.laudien.p1xelfehler.batterywarner.RootChecker;
 import com.laudien.p1xelfehler.batterywarner.Services.ChargingService;
+
+import static com.laudien.p1xelfehler.batterywarner.NotificationBuilder.ID_NOT_ROOTED;
 
 public class ChargingReceiver extends BroadcastReceiver {
     //private final String TAG = getClass().getSimpleName();
@@ -40,6 +44,17 @@ public class ChargingReceiver extends BroadcastReceiver {
             return;
         }
         boolean usbDisabled = sharedPreferences.getBoolean(context.getString(R.string.pref_usb_charging_disabled), context.getResources().getBoolean(R.bool.pref_usb_charging_disabled_default));
+        boolean stopChargingEnabled = sharedPreferences.getBoolean(context.getString(R.string.pref_stop_charging), context.getResources().getBoolean(R.bool.pref_stop_charging_default));
+        if (usbDisabled || stopChargingEnabled) { // if any root feature is enabled
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    if (!RootChecker.isDeviceRooted()) { // show notification if the device is not rooted anymore!
+                        NotificationBuilder.showNotification(context, ID_NOT_ROOTED);
+                    }
+                }
+            });
+        }
         if (ChargingService.isChargingTypeEnabled(context, batteryStatus) || usbDisabled) {
             // start service
             ChargingService.startService(context);
