c9886a2ce7ff398b280792cae9e23ea442a45c9a
pixelfehler1996 <checkercraft1996@gmail.com>
2017-03-07 15:35:57 +0100

    improved "not rooted anymore"-feature

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/GrantRootReceiver.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Receivers/GrantRootReceiver.java
@@ -3,11 +3,15 @@ package com.laudien.p1xelfehler.batterywarner.Receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
-import android.preference.PreferenceManager;
 
-import com.laudien.p1xelfehler.batterywarner.R;
+import com.laudien.p1xelfehler.batterywarner.Contract;
+import com.laudien.p1xelfehler.batterywarner.NotificationBuilder;
 import com.laudien.p1xelfehler.batterywarner.RootChecker;
+
+import static com.laudien.p1xelfehler.batterywarner.NotificationBuilder.ID_NOT_ROOTED;
+import static com.laudien.p1xelfehler.batterywarner.NotificationBuilder.ID_STOP_CHARGING;
 
 public class GrantRootReceiver extends BroadcastReceiver {
     @Override
@@ -22,10 +26,31 @@ public class GrantRootReceiver extends BroadcastReceiver {
             @Override
             protected void onPostExecute(Boolean rooted) {
                 super.onPostExecute(rooted);
-                if (rooted) {
-                    PreferenceManager.getDefaultSharedPreferences(context).edit()
-                            .putBoolean(context.getString(R.string.pref_stop_charging), true).apply();
-                } else {
+                if (rooted) { // rooting was allowed now
+                    Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+                    if (batteryStatus != null) {
+                        boolean isCharging = batteryStatus.getIntExtra(android.os.BatteryManager.EXTRA_PLUGGED, Contract.NO_STATE) != 0;
+                        if (!isCharging) { // if not charging make sure that it is not disabled by the app
+                            AsyncTask.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        boolean chargingEnabled = RootChecker.isChargingEnabled();
+                                        if (!chargingEnabled) { // if disabled by app, show notification!
+                                            NotificationBuilder.showNotification(context, ID_STOP_CHARGING);
+                                        }
+                                    } catch (RootChecker.NotRootedException e) { // user disabled root again after allowing it
+                                        e.printStackTrace();
+                                        NotificationBuilder.showNotification(context, ID_NOT_ROOTED);
+                                    } catch (RootChecker.BatteryFileNotFoundException e) {
+                                        // Should not happen! Is checked before the user can enable the feature!
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                } else { // user is stupid and keeps root disabled -> disable all root features
                     context.sendBroadcast(new Intent(context, DisableRootFeaturesReceiver.class));
                 }
             }
