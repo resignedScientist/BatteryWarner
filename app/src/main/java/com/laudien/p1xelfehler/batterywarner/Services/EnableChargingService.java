8aaa41648752ffe9f943b2bf94350e60c7b0cef3
pixelfehler1996 <checkercraft1996@gmail.com>
2017-03-09 11:06:06 +0100

    changed EnableChargingService to be an IntentService

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Services/EnableChargingService.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Services/EnableChargingService.java
@@ -1,5 +1,6 @@
 package com.laudien.p1xelfehler.batterywarner.Services;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -12,32 +13,29 @@ import com.laudien.p1xelfehler.batterywarner.RootChecker;
 import static com.laudien.p1xelfehler.batterywarner.NotificationBuilder.ID_NOT_ROOTED;
 
 /**
- * A Service called by the app that enables the charging again.
+ * An IntentService called by the app that enables the charging again.
  * If the device is not rooted anymore, the notification with the id
  * ID_NOT_ROOTED will be triggered.
- * It stops itself after it finished.
+ * It stops itself after it finished (like every IntentService does!).
  */
-public class EnableChargingService extends Service {
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    RootChecker.enableCharging(EnableChargingService.this);
-                } catch (RootChecker.NotRootedException e) {
-                    NotificationBuilder.showNotification(EnableChargingService.this, ID_NOT_ROOTED);
-                    e.printStackTrace();
-                }
-            }
-        });
-        stopSelf();
-        return super.onStartCommand(intent, flags, startId);
+public class EnableChargingService extends IntentService {
+    public EnableChargingService(String name) {
+        super(name);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        try {
+            RootChecker.enableCharging(EnableChargingService.this);
+        } catch (RootChecker.NotRootedException e) {
+            NotificationBuilder.showNotification(EnableChargingService.this, ID_NOT_ROOTED);
+            e.printStackTrace();
+        }
     }
 }
