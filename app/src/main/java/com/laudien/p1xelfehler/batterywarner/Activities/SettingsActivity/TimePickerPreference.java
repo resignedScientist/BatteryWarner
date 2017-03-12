26ba8bc373307d3f206e4d04cc576fdc43683873
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-03-12 09:10:37 +0100

    begin to implement time picker preference

---
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SettingsActivity/TimePickerPreference.java
@@ -0,0 +1,60 @@
+package com.laudien.p1xelfehler.batterywarner.Activities.SettingsActivity;
+
+import android.content.Context;
+import android.os.Build;
+import android.preference.DialogPreference;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TimePicker;
+
+public class TimePickerPreference extends DialogPreference {
+
+    private TimePicker timePicker = null;
+    private int hour, minute;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public TimePickerPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public TimePickerPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TimePickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public TimePickerPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        timePicker = new TimePicker(getContext());
+        if (hour != 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                timePicker.setHour(hour);
+                timePicker.setMinute(minute);
+            } else {
+                timePicker.setCurrentHour(hour);
+                timePicker.setCurrentMinute(minute);
+            }
+        }
+        return timePicker;
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            hour = timePicker.getHour();
+            minute = timePicker.getMinute();
+        } else {
+            hour = timePicker.getCurrentHour();
+            minute = timePicker.getCurrentMinute();
+        }
+    }
+}
