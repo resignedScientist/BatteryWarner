9abc9445875d6efd1053ea347aba25366f83f225
pixelfehler1996 <checkercraft1996@gmail.com>
2017-03-13 17:07:17 +0100

    almost fully integrated smart charging preferences

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SmartChargingActivity/SmartChargingFragment.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/Activities/SmartChargingActivity/SmartChargingFragment.java
@@ -1,19 +1,56 @@
 package com.laudien.p1xelfehler.batterywarner.Activities.SmartChargingActivity;
 
-import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.laudien.p1xelfehler.batterywarner.R;
 
-public class SmartChargingFragment extends Fragment {
-    @Nullable
+public class SmartChargingPreferenceFragment extends PreferenceFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_smart_charging, container, false);
-        return view;
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.smart_charging);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.smart_charging_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_info){
+            openInfoDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openInfoDialog() {
+        Context context = getActivity();
+        if (context != null) {
+            new AlertDialog.Builder(context)
+                    .setTitle("Information")
+                    .setView(R.layout.fragment_smart_charging)
+                    .setCancelable(true)
+                    .setPositiveButton(R.string.ok, null)
+                    .setIcon(R.mipmap.ic_launcher)
+                    .create()
+                    .show();
+        }
     }
 }
