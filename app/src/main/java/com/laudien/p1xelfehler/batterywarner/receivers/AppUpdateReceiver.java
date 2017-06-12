32db9f7062b58f1e862cbc7529b738bda2d8b4f6
pixelfehler1996 <checkercraft1996@gmail.com>
2017-06-12 09:42:05 +0200

    removed patching of old time strings (no one uses these old versions!)

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/receivers/AppUpdateReceiver.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/receivers/AppUpdateReceiver.java
@@ -6,25 +6,16 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 import com.laudien.p1xelfehler.batterywarner.R;
 import com.laudien.p1xelfehler.batterywarner.helper.NotificationHelper;
 import com.laudien.p1xelfehler.batterywarner.services.ChargingService;
 import com.laudien.p1xelfehler.batterywarner.services.DischargingService;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-
 import static android.content.Context.MODE_PRIVATE;
 import static android.content.Intent.ACTION_BATTERY_CHANGED;
 import static android.os.BatteryManager.EXTRA_PLUGGED;
 import static com.laudien.p1xelfehler.batterywarner.helper.NotificationHelper.ID_GRANT_ROOT;
-import static java.text.DateFormat.SHORT;
-import static java.util.Calendar.HOUR_OF_DAY;
-import static java.util.Calendar.MINUTE;
 
 /**
  * A BroadcastReceiver that is called by the system if the app has been updated.
@@ -55,26 +46,6 @@ public class AppUpdateReceiver extends BroadcastReceiver {
             }
             if (isCharging) { // charging -> start ChargingService
                 context.startService(new Intent(context, ChargingService.class));
-            }
-            // patch old time strings
-            try {
-                String timeString = sharedPreferences.getString(context.getString(R.string.pref_smart_charging_time), null);
-                if (timeString != null) {
-                    DateFormat dateFormat = java.text.DateFormat.getTimeInstance(SHORT, Locale.getDefault());
-                    Date date = dateFormat.parse(timeString);
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.setTime(date);
-                    int hours = calendar.get(HOUR_OF_DAY);
-                    int minutes = calendar.get(MINUTE);
-                    calendar = Calendar.getInstance();
-                    calendar.set(HOUR_OF_DAY, hours);
-                    calendar.set(MINUTE, minutes);
-                    Log.d(getClass().getSimpleName(), date.toString());
-                    sharedPreferences.edit().remove(context.getString(R.string.pref_smart_charging_time))
-                            .putLong(context.getString(R.string.pref_smart_charging_time), calendar.getTimeInMillis())
-                            .apply();
-                }
-            } catch (Exception ignored) {
             }
             // patch old shared preferences =>
             SharedPreferences temporaryPrefs = context.getSharedPreferences(context.getString(R.string.prefs_temporary), MODE_PRIVATE);
