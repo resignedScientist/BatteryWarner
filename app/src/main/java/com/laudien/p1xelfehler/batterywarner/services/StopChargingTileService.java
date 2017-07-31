7a49a2339a5bab6dfe569c504db81225bebb00d9
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-07-31 23:36:33 +0200

    changed tile to stop charging tile

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/services/StopChargingTileService.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/services/StopChargingTileService.java
@@ -14,13 +14,13 @@ import com.laudien.p1xelfehler.batterywarner.helper.NotificationHelper;
 import com.laudien.p1xelfehler.batterywarner.helper.RootHelper;
 import com.laudien.p1xelfehler.batterywarner.helper.ToastHelper;
 
-import static android.widget.Toast.LENGTH_LONG;
 import static android.widget.Toast.LENGTH_SHORT;
 import static com.laudien.p1xelfehler.batterywarner.AppInfoHelper.IS_PRO;
+import static com.laudien.p1xelfehler.batterywarner.helper.NotificationHelper.ID_NOT_ROOTED;
 import static com.laudien.p1xelfehler.batterywarner.helper.NotificationHelper.ID_STOP_CHARGING_NOT_WORKING;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
-public class ToggleChargingTileService extends TileService {
+public class StopChargingTileService extends TileService {
 
     private final byte RESULT_NOT_ROOTED = 1;
     private final byte RESULT_STOP_CHARGING_NOT_WORKING = 2;
@@ -35,9 +35,9 @@ public class ToggleChargingTileService extends TileService {
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean stopChargingEnabled = sharedPreferences.getBoolean(getString(R.string.pref_stop_charging), getResources().getBoolean(R.bool.pref_stop_charging_default));
         if (stopChargingEnabled) {
-            tile.setState(Tile.STATE_INACTIVE);
-        } else {
             tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
         }
         tile.updateTile();
     }
@@ -48,17 +48,26 @@ public class ToggleChargingTileService extends TileService {
         super.onClick();
         boolean isActive = tile.getState() == Tile.STATE_ACTIVE;
         if (isActive) { // deactivating the tile
+            tile.setState(Tile.STATE_INACTIVE);
+            sharedPreferences.edit()
+                    .putBoolean(getString(R.string.pref_stop_charging), false)
+                    .apply();
+        } else { // activating the tile
             if (IS_PRO) {
+                tile.setState(Tile.STATE_INACTIVE);
                 new AsyncTask<Void, Void, Byte>() {
                     @Override
                     protected Byte doInBackground(Void... voids) {
                         try {
                             RootHelper.isChargingEnabled();
+                            sharedPreferences.edit()
+                                    .putBoolean(getString(R.string.pref_stop_charging), true)
+                                    .apply();
                             return RESULT_OK;
                         } catch (RootHelper.NotRootedException e) {
                             return RESULT_NOT_ROOTED;
                         } catch (RootHelper.NoBatteryFileFoundException e) {
-                            NotificationHelper.showNotification(ToggleChargingTileService.this, ID_STOP_CHARGING_NOT_WORKING);
+                            NotificationHelper.showNotification(StopChargingTileService.this, ID_STOP_CHARGING_NOT_WORKING);
                             return RESULT_STOP_CHARGING_NOT_WORKING;
                         }
                     }
@@ -66,27 +75,18 @@ public class ToggleChargingTileService extends TileService {
                     @Override
                     protected void onPostExecute(Byte b) {
                         super.onPostExecute(b);
-                        switch (b) {
-                            case RESULT_NOT_ROOTED:
-                                ToastHelper.sendToast(ToggleChargingTileService.this, R.string.toast_not_rooted, LENGTH_LONG);
-                                break;
-                            case RESULT_STOP_CHARGING_NOT_WORKING:
-                                NotificationHelper.showNotification(ToggleChargingTileService.this, ID_STOP_CHARGING_NOT_WORKING);
-                            case RESULT_OK:
-                                tile.setState(Tile.STATE_INACTIVE);
-                                tile.updateTile();
-                                sharedPreferences.edit().putBoolean(getString(R.string.pref_stop_charging), true).apply();
-                                break;
+                        if (b == RESULT_NOT_ROOTED || b == RESULT_STOP_CHARGING_NOT_WORKING) {
+                            tile.setState(Tile.STATE_INACTIVE);
+                            tile.updateTile();
+                            int notificationId = b == RESULT_NOT_ROOTED ? ID_NOT_ROOTED : ID_STOP_CHARGING_NOT_WORKING;
+                            NotificationHelper.showNotification(StopChargingTileService.this, notificationId);
                         }
                     }
                 }.execute();
             } else {
                 ToastHelper.sendToast(getApplicationContext(), R.string.toast_not_pro, LENGTH_SHORT);
             }
-        } else { // activating the tile
-            tile.setState(Tile.STATE_ACTIVE);
-            sharedPreferences.edit().putBoolean(getString(R.string.pref_stop_charging), false).apply();
-            tile.updateTile();
         }
+        tile.updateTile();
     }
 }
