99797cd0382f830857cbe6c9cc01e612ceb6cbdb
pixelfehler1996 <norman.laudien1996@gmail.com>
2017-08-02 11:34:22 +0200

    removed old code

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/helper/NotificationHelper.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/helper/NotificationHelper.java
@@ -9,8 +9,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationManagerCompat;
@@ -19,65 +17,38 @@ import com.laudien.p1xelfehler.batterywarner.MainActivity;
 import com.laudien.p1xelfehler.batterywarner.R;
 import com.laudien.p1xelfehler.batterywarner.SettingsActivity;
 import com.laudien.p1xelfehler.batterywarner.preferences.smartChargingActivity.SmartChargingActivity;
-import com.laudien.p1xelfehler.batterywarner.services.EnableChargingService;
 import com.laudien.p1xelfehler.batterywarner.services.GrantRootService;
-import com.laudien.p1xelfehler.batterywarner.services.TogglePowerSavingService;
-
-import java.util.Locale;
 
 import static android.app.Notification.PRIORITY_HIGH;
-import static android.app.Notification.PRIORITY_LOW;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 import static android.content.Context.NOTIFICATION_SERVICE;
 import static android.media.RingtoneManager.TYPE_NOTIFICATION;
 import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.LOLLIPOP;
 import static android.os.Build.VERSION_CODES.O;
 
 /**
  * Helper class to show a notification with the given type. All notifications used in the app are listed here.
  */
 public final class NotificationHelper {
-    /**
-     * Notification id of the notification that warns if silent/vibrate mode is turned on.
-     */
-    public static final int ID_SILENT_MODE = 1001;
-    /**
-     * Notification id of the notification that warns that the battery is above X%.
-     */
-    public static final int ID_WARNING_HIGH = 1002;
-    /**
-     * Notification id of the notification that warns that the battery is below Y%.
-     */
-    public static final int ID_WARNING_LOW = 1003;
-    /**
-     * Notification id of the notification that the user has to click/dismiss after the device is unplugged.
-     * Only shown if the stop charging feature is enabled.
-     */
-    public static final int ID_STOP_CHARGING = 1004;
-    /**
-     * Notification id of the notification that asks the user for root again after the app was updated.
-     */
-    public static final int ID_GRANT_ROOT = 1005;
+    public static final int ID_GRANT_ROOT = 1001;
     /**
      * Notification id of the notification that tells the user that the stop charging feature is not working
      * on this device.
      */
-    public static final int ID_STOP_CHARGING_NOT_WORKING = 1006;
+    public static final int ID_STOP_CHARGING_NOT_WORKING = 1002;
     /**
      * Notification id of the notification that asks for root again if app has no root rights anymore.
      */
-    public static final int ID_NOT_ROOTED = 1007;
+    public static final int ID_NOT_ROOTED = 1003;
     /**
      * Notification id of the notification that tells the user that no alarm was found in the alarm app
      **/
-    public static final int ID_NO_ALARM_TIME_FOUND = 1008;
+    public static final int ID_NO_ALARM_TIME_FOUND = 1004;
     /**
      * Notification id that is shown on certain special events like sales.
      * It is the only notification that cannot be used with showNotification().
      * Use showEventNotification() instead!
      */
-    public static final int ID_EVENT = 1009;
 
     public static final long[] VIBRATE_PATTERN = {0, 300, 300, 300};
 
@@ -95,18 +66,6 @@ public final class NotificationHelper {
     public static void showNotification(final Context context, final int notificationID) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         switch (notificationID) {
-            case ID_WARNING_HIGH:
-                showWarningHighNotification(context, sharedPreferences);
-                break;
-            case ID_WARNING_LOW:
-                showWarningLowNotification(context, sharedPreferences);
-                break;
-            case ID_SILENT_MODE:
-                showSilentModeNotification(context);
-                break;
-            case ID_STOP_CHARGING:
-                showStopChargingNotification(context, sharedPreferences);
-                break;
             case ID_STOP_CHARGING_NOT_WORKING:
                 showStopChargingNotWorkingNotification(context, sharedPreferences);
                 break;
@@ -124,32 +83,6 @@ public final class NotificationHelper {
         }
     }
 
-    public static void showEventNotification(Context context, String title, String message, String buttonText, Intent buttonIntent) {
-        Notification.Builder builder = new Notification.Builder(context)
-                .setSmallIcon(SMALL_ICON_RESOURCE)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setStyle(getBigTextStyle(message))
-                .setAutoCancel(true);
-        if (buttonIntent != null && buttonText != null && !buttonText.equals("")) {
-            PendingIntent pendingIntent = PendingIntent.getService(context, ID_EVENT, buttonIntent, 0);
-            builder.addAction(0, buttonText, pendingIntent)
-                    .setContentIntent(pendingIntent);
-        } else {
-            builder.setContentIntent(getDefaultClickIntent(context));
-        }
-        if (SDK_INT >= O) {
-            builder.setChannelId(context.getString(R.string.channel_other_warnings));
-        } else {
-            builder.setPriority(Notification.PRIORITY_DEFAULT)
-                    .setSound(getDefaultSound())
-                    .setVibrate(VIBRATE_PATTERN);
-        }
-        NotificationManager notificationManager = (NotificationManager)
-                context.getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(ID_EVENT, builder.build());
-    }
-
     /**
      * Cancels the notification with the given notification id.
      *
@@ -160,158 +93,6 @@ public final class NotificationHelper {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
         for (int id : notificationID) {
             notificationManager.cancel(id);
-        }
-    }
-
-    private static void showWarningHighNotification(final Context context, SharedPreferences defaultPrefs) {
-        boolean warningHighEnabled = defaultPrefs.getBoolean(context.getString(R.string.pref_warning_high_enabled), context.getResources().getBoolean(R.bool.pref_warning_high_enabled_default));
-        boolean smartChargingEnabled = defaultPrefs.getBoolean(context.getString(R.string.pref_smart_charging_enabled), context.getResources().getBoolean(R.bool.pref_smart_charging_enabled_default));
-        boolean resetBatteryStats = !smartChargingEnabled && defaultPrefs.getBoolean(context.getString(R.string.pref_reset_battery_stats), context.getResources().getBoolean(R.bool.pref_reset_battery_stats_default));
-        // show notification
-        if (warningHighEnabled) {
-            int warningHigh = defaultPrefs.getInt(context.getString(R.string.pref_warning_high), context.getResources().getInteger(R.integer.pref_warning_high_default));
-            String messageText = String.format(Locale.getDefault(), "%s %d%%!", context.getString(R.string.notification_warning_high), warningHigh);
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-            Notification.Builder builder = new Notification.Builder(context)
-                    .setSmallIcon(SMALL_ICON_RESOURCE)
-                    .setContentTitle(context.getString(R.string.app_name))
-                    .setContentText(messageText)
-                    .setStyle(getBigTextStyle(messageText))
-                    .setContentIntent(getDefaultClickIntent(context))
-                    .setAutoCancel(true)
-                    .setSound(getWarningSound(context, defaultPrefs, true))
-                    .setVibrate(VIBRATE_PATTERN);
-            if (Build.VERSION.SDK_INT >= O) {
-                builder.setChannelId(context.getString(R.string.channel_battery_warnings));
-            } else {
-                builder.setPriority(PRIORITY_HIGH);
-            }
-            notificationManager.notify(ID_WARNING_HIGH, builder.build());
-            // reset the android internal battery stats
-            if (resetBatteryStats) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            RootHelper.resetBatteryStats();
-                        } catch (RootHelper.NotRootedException e) {
-                            e.printStackTrace();
-                            showNotRootedNotification(context);
-                        }
-                    }
-                });
-            }
-        }
-    }
-
-    private static void showWarningLowNotification(final Context context, SharedPreferences sharedPreferences) {
-        boolean warningLowEnabled = sharedPreferences.getBoolean(context.getString(R.string.pref_warning_low_enabled), context.getResources().getBoolean(R.bool.pref_warning_low_enabled_default));
-        boolean prefPowerSavingModeEnabled = SDK_INT >= LOLLIPOP && sharedPreferences.getBoolean(context.getString(R.string.pref_power_saving_mode), context.getResources().getBoolean(R.bool.pref_power_saving_mode_default));
-        if (warningLowEnabled) {
-            int warningLow = sharedPreferences.getInt(context.getString(R.string.pref_warning_low), context.getResources().getInteger(R.integer.pref_warning_low_default));
-            String messageText = String.format(Locale.getDefault(), "%s %d%%!", context.getString(R.string.notification_warning_low), warningLow);
-            Notification.Builder builder = new Notification.Builder(context)
-                    .setSmallIcon(SMALL_ICON_RESOURCE)
-                    .setContentTitle(context.getString(R.string.app_name))
-                    .setContentText(messageText)
-                    .setStyle(getBigTextStyle(messageText))
-                    .setContentIntent(getDefaultClickIntent(context))
-                    .setAutoCancel(true)
-                    .setSound(getWarningSound(context, sharedPreferences, false))
-                    .setVibrate(VIBRATE_PATTERN);
-            if (Build.VERSION.SDK_INT >= O) {
-                builder.setChannelId(context.getString(R.string.channel_battery_warnings));
-            } else {
-                builder.setPriority(PRIORITY_HIGH);
-            }
-            // enable power saving mode
-            if (SDK_INT >= LOLLIPOP && prefPowerSavingModeEnabled) {
-                AsyncTask.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            RootHelper.togglePowerSavingMode(true);
-                        } catch (RootHelper.NotRootedException e) {
-                            e.printStackTrace();
-                            showNotRootedNotification(context);
-                        }
-                    }
-                });
-                Intent exitPowerSaveIntent = new Intent(context, TogglePowerSavingService.class);
-                PendingIntent pendingIntent = PendingIntent.getService(context, 0, exitPowerSaveIntent, 0);
-                builder.addAction(R.drawable.ic_battery_20_white_24dp, context.getString(R.string.notification_button_toggle_power_saving), pendingIntent);
-            }
-            // build and show notification
-            NotificationManager notificationManager = (NotificationManager)
-                    context.getSystemService(NOTIFICATION_SERVICE);
-            notificationManager.notify(ID_WARNING_LOW, builder.build());
-        }
-    }
-
-    private static void showSilentModeNotification(Context context) {
-        String messageText = context.getString(R.string.notification_sound_disabled);
-        Notification.Builder builder = new Notification.Builder(context)
-                .setSmallIcon(SMALL_ICON_RESOURCE)
-                .setContentTitle(context.getString(R.string.app_name))
-                .setContentText(messageText)
-                .setStyle(getBigTextStyle(messageText))
-                .setContentIntent(getDefaultClickIntent(context))
-                .setAutoCancel(true)
-                .setSound(getDefaultSound())
-                .setVibrate(VIBRATE_PATTERN);
-        if (SDK_INT >= O) {
-            builder.setChannelId(context.getString(R.string.channel_other_warnings));
-        } else {
-            builder.setPriority(PRIORITY_HIGH);
-        }
-        NotificationManager notificationManager = (NotificationManager)
-                context.getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(ID_SILENT_MODE, builder.build());
-    }
-
-    private static void showStopChargingNotification(final Context context, SharedPreferences sharedPreferences) {
-        boolean stopChargingEnabled = sharedPreferences.getBoolean(context.getString(R.string.pref_stop_charging), context.getResources().getBoolean(R.bool.pref_stop_charging_default));
-        boolean usbChargingDisabled = sharedPreferences.getBoolean(context.getString(R.string.pref_usb_charging_disabled), context.getResources().getBoolean(R.bool.pref_usb_charging_disabled_default));
-        final boolean resetBatteryStats = sharedPreferences.getBoolean(context.getString(R.string.pref_reset_battery_stats), context.getResources().getBoolean(R.bool.pref_reset_battery_stats_default));
-        if (stopChargingEnabled || usbChargingDisabled) {
-            AsyncTask.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        if (!RootHelper.isChargingEnabled()) {
-                            PendingIntent pendingIntent = PendingIntent.getService(context, ID_STOP_CHARGING,
-                                    new Intent(context, EnableChargingService.class), PendingIntent.FLAG_CANCEL_CURRENT);
-                            String messageText = context.getString(R.string.notification_charging_disabled);
-                            Notification.Builder builder = new Notification.Builder(context)
-                                    .setSmallIcon(SMALL_ICON_RESOURCE)
-                                    .setContentTitle(context.getString(R.string.app_name))
-                                    .setContentText(messageText)
-                                    .setStyle(getBigTextStyle(messageText))
-                                    .setContentIntent(pendingIntent)
-                                    .addAction(R.drawable.ic_battery_charging_full_white_24dp, context.getString(R.string.notification_button_enable_charging), pendingIntent)
-                                    .setOngoing(true);
-                            if (SDK_INT >= O) {
-                                builder.setChannelId(context.getString(R.string.channel_battery_warnings));
-                            } else {
-                                builder.setPriority(PRIORITY_LOW);
-                            }
-                            NotificationManager notificationManager = (NotificationManager)
-                                    context.getSystemService(NOTIFICATION_SERVICE);
-                            notificationManager.notify(ID_STOP_CHARGING, builder.build());
-                            // reset the android internal battery stats
-                            if (resetBatteryStats) {
-                                RootHelper.resetBatteryStats();
-                            }
-                        }
-                    } catch (RootHelper.NotRootedException e) {
-                        e.printStackTrace();
-                        showNotification(context, ID_NOT_ROOTED);
-                    } catch (RootHelper.NoBatteryFileFoundException e) {
-                        e.printStackTrace();
-                        showNotification(context, ID_STOP_CHARGING_NOT_WORKING);
-                    }
-                }
-            });
         }
     }
 
