4a80c1f1308bcfd18b900d36283d480764e729f2
pixelfehler1996 <checkercraft1996@gmail.com>
2017-08-11 09:46:18 +0200

    make the DatabaseContract package-private

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/database/DatabaseContract.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/database/DatabaseContract.java
@@ -1,8 +1,8 @@
 package com.laudien.p1xelfehler.batterywarner.database;
 
-public class DatabaseContract {
-    public static final String TABLE_NAME = "ChargeCurve";
-    public static final String TABLE_COLUMN_TIME = "time";
-    public static final String TABLE_COLUMN_PERCENTAGE = "percentage";
-    public static final String TABLE_COLUMN_TEMP = "temperature";
+class DatabaseContract {
+    static final String TABLE_NAME = "ChargeCurve";
+    static final String TABLE_COLUMN_TIME = "time";
+    static final String TABLE_COLUMN_PERCENTAGE = "percentage";
+    static final String TABLE_COLUMN_TEMP = "temperature";
 }
