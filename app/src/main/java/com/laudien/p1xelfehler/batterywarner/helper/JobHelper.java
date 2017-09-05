1419cc3d6bc2328b065192b5480a5fdd34c153e1
pixelfehler1996 <checkercraft1996@gmail.com>
2017-09-05 10:15:02 +0200

    added GraphAutoDeleteService

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/helper/JobHelper.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/helper/JobHelper.java
@@ -1,22 +1,52 @@
 package com.laudien.p1xelfehler.batterywarner.helper;
 
+import android.Manifest;
 import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.laudien.p1xelfehler.batterywarner.services.GraphAutoDeleteService;
 
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
 @RequiresApi(LOLLIPOP)
 public class JobHelper {
-    private static final int JOB_ID_GRAPH_AUTO_DELETE = 1337;
+    private static final int ID_AUTO_DELETE_GRAPHS = 1337;
+    private static final String TAG = JobHelper.class.getSimpleName();
 
-    public void schedule (int jobId){
-        if (jobId == JOB_ID_GRAPH_AUTO_DELETE){
-            scheduleGraphAutoDelete();
+    public static void schedule(Context context, int jobId) {
+        if (jobId == ID_AUTO_DELETE_GRAPHS) {
+            scheduleGraphAutoDelete(context);
         }
     }
 
-    private void scheduleGraphAutoDelete(){
-        
+    public static void cancel(Context context, int jobId) {
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        jobScheduler.cancel(jobId);
+        Log.d(TAG, "Job canceled: " + jobId);
+    }
+
+    private static void scheduleGraphAutoDelete(Context context) {
+        int permissionCheck = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            ComponentName componentName = new ComponentName(context, GraphAutoDeleteService.class);
+            JobInfo jobInfo =
+                    new JobInfo.Builder(ID_AUTO_DELETE_GRAPHS, componentName)
+                            .setRequiresCharging(true)
+                            .setRequiresDeviceIdle(true)
+                            .setPeriodic(1000 * 60 * 60 * 24)
+                            .setPersisted(true) // survive reboots
+                            .build();
+            boolean result = jobScheduler.schedule(jobInfo) == JobScheduler.RESULT_SUCCESS;
+            Log.d(TAG, "Job scheduled, success: " + result);
+        } else {
+            Log.d(TAG, "Scheduling job failed! No storage permission granted!");
+        }
     }
 }
---
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/services/GraphAutoDeleteService.java
@@ -0,0 +1,19 @@
+package com.laudien.p1xelfehler.batterywarner.services;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class GraphAutoDeleteService extends JobService {
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return false;
+    }
+}
