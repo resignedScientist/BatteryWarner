c54e71de640fab72c4ba89596c4581f3f1d44403
pixelfehler1996 <checkercraft1996@gmail.com>
2017-09-06 13:40:48 +0200

    fixed crash/freeze with tile when checking root

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/tiles/PreferenceTileService.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/tiles/PreferenceTileService.java
@@ -1,47 +1,56 @@
 package com.laudien.p1xelfehler.batterywarner.tiles;
 
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.support.annotation.RequiresApi;
 
-import eu.chainfire.libsuperuser.Shell;
+import com.laudien.p1xelfehler.batterywarner.helper.RootHelper;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public abstract class PreferenceTileService extends TileService {
     private Tile tile;
     private SharedPreferences sharedPreferences;
     private String key;
-    private boolean rootAvailable;
 
     @Override
     public void onStartListening() {
         super.onStartListening();
-        rootAvailable = !requiresRoot() || Shell.SU.available();
         tile = getQsTile();
-        if (rootAvailable) {
-            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-            key = getString(getPreferenceKeyStringResource());
-            boolean defaultBool = getResources().getBoolean(getPreferenceDefaultBoolResource());
-            boolean enabled = sharedPreferences.getBoolean(key, defaultBool);
-            tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-        } else {
-            tile.setState(Tile.STATE_UNAVAILABLE);
-        }
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        key = getString(getPreferenceKeyStringResource());
+        boolean defaultBool = getResources().getBoolean(getPreferenceDefaultBoolResource());
+        boolean enabled = sharedPreferences.getBoolean(key, defaultBool);
+        tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         tile.updateTile();
+
+        // check for root
+        if (requiresRoot()) {
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    if (!RootHelper.isRootAvailable()) {
+                        try {
+                            tile.setState(Tile.STATE_UNAVAILABLE);
+                            tile.updateTile();
+                        } catch (Exception ignored) {
+                        }
+                    }
+                }
+            });
+        }
     }
 
     @Override
     public void onClick() {
         super.onClick();
-        if (rootAvailable) {
-            boolean isActive = tile.getState() == Tile.STATE_ACTIVE;
-            sharedPreferences.edit().putBoolean(key, !isActive).apply();
-            tile.setState(isActive ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
-            tile.updateTile();
-        }
+        boolean isActive = tile.getState() == Tile.STATE_ACTIVE;
+        sharedPreferences.edit().putBoolean(key, !isActive).apply();
+        tile.setState(isActive ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        tile.updateTile();
     }
 
     protected abstract int getPreferenceKeyStringResource();
