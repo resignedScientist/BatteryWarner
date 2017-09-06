f07f26e8ba6f040c4a06abd0f46fe6d81a6694ad
pixelfehler1996 <checkercraft1996@gmail.com>
2017-09-06 10:27:06 +0200

    changed log message, removed toast

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/receivers/TaskerFireReceiver.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/receivers/TaskerFireReceiver.java
@@ -8,7 +8,6 @@ import android.util.Log;
 
 import com.laudien.p1xelfehler.batterywarner.helper.ServiceHelper;
 import com.laudien.p1xelfehler.batterywarner.helper.TaskerHelper;
-import com.laudien.p1xelfehler.batterywarner.helper.ToastHelper;
 import com.laudien.p1xelfehler.batterywarner.services.EnableChargingService;
 import com.twofortyfouram.locale.sdk.client.receiver.AbstractPluginSettingReceiver;
 
@@ -27,13 +26,12 @@ public class TaskerFireReceiver extends AbstractPluginSettingReceiver {
     @Override
     protected void firePluginSetting(@NonNull final Context context, @NonNull Bundle bundle) {
         final boolean charging = TaskerHelper.getBundleResult(bundle);
+        Log.d(getClass().getSimpleName(), "Tasker Plugin fired! Charging = " + charging);
+
         if (charging) {
             // do the same as the "Enable charging" button on the stop charging notification
             Intent intent = new Intent(context.getApplicationContext(), EnableChargingService.class);
             ServiceHelper.startService(context.getApplicationContext(), intent);
         }
-
-        ToastHelper.sendToast(context, "Plugin fired! :)");
-        Log.d(getClass().getSimpleName(), "Plugin fired! :)");
     }
 }
