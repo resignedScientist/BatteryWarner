64136479f2575c6bb619046d36514197a709a4f9
Norman Laudien <norman.laudien1996@gmail.com>
2017-11-17 13:20:54 +0100

    testing reverseCurrent in notifyValueAdded()

--- app/src/test/java/com/laudien/p1xelfehler/batterywarner/database/DatabaseControllerTest.java
+++ app/src/test/java/com/laudien/p1xelfehler/batterywarner/database/DatabaseControllerTest.java
@@ -198,6 +198,28 @@ public class DatabaseControllerTest {
         }
     }
 
+    @Test
+    public void notifyValueAdded3() {
+        // test if the current is reversed correctly
+        DatabaseValue databaseValue = new DatabaseValue(80, 205, 3500, -2100, 60000);
+        databaseController.notifyValueAdded(databaseValue, 1, true);
+        verify(databaseListener).onValueAdded(captor.capture(), Mockito.anyLong());
+
+        // check the DataPoints
+        DataPoint[] receivedDataPoints = captor.getValue();
+        assertNotNull(receivedDataPoints);
+        assertEquals(NUMBER_OF_GRAPHS, receivedDataPoints.length);
+        assertEquals(80d, receivedDataPoints[GRAPH_INDEX_BATTERY_LEVEL].getY(), 0d);
+        assertEquals(20.5, receivedDataPoints[GRAPH_INDEX_TEMPERATURE].getY(), 0d);
+        assertEquals(3.5, receivedDataPoints[GRAPH_INDEX_VOLTAGE].getY(), 0d);
+        assertEquals(-2.1, receivedDataPoints[GRAPH_INDEX_CURRENT].getY(), 0d);
+
+        // time (= x value) must be 0
+        for (int i = 0; i < NUMBER_OF_GRAPHS; i++) {
+            assertEquals(0d, receivedDataPoints[i].getX(), 0d);
+        }
+    }
+
     private int getSize(LineGraphSeries graph) {
         Iterator<LineGraphSeries> iterator = graph.getValues(0d, Double.MAX_VALUE);
         int size = 0;
