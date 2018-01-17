656e56460221b11f387d4d21d4435fb9113d8134
Norman Laudien <norman.laudien1996@gmail.com>
2018-01-17 11:39:50 +0100

    extract tasks into own methods

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/database/DatabaseUtils.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/database/DatabaseUtils.java
@@ -148,6 +148,81 @@ public final class DatabaseUtils {
         }
     }
 
+    @Nullable
+    static LineGraphSeries<DataPoint>[] generateGraphsTask(@NonNull DatabaseValue[] databaseValues, boolean useFahrenheit, boolean reverseCurrent) {
+        if (databaseValues[0] == null) {
+            return null;
+        }
+        LineGraphSeries<DataPoint>[] graphs = new LineGraphSeries[NUMBER_OF_GRAPHS];
+        graphs[GRAPH_INDEX_BATTERY_LEVEL] = new LineGraphSeries();
+        graphs[GRAPH_INDEX_TEMPERATURE] = new LineGraphSeries();
+        if (databaseValues[0].getVoltage() != 0)
+            graphs[GRAPH_INDEX_VOLTAGE] = new LineGraphSeries();
+        if (databaseValues[0].getCurrent() != 0)
+            graphs[GRAPH_INDEX_CURRENT] = new LineGraphSeries();
+        for (DatabaseValue value : databaseValues) {
+            DataPoint[] dataPoints = value.toDataPoints(useFahrenheit, reverseCurrent);
+            for (int i = 0; i < NUMBER_OF_GRAPHS; i++) {
+                if (graphs[i] != null && dataPoints[i] != null) {
+                    graphs[i].appendData(dataPoints[i], false, databaseValues.length);
+                }
+            }
+        }
+        return graphs;
+    }
+
+    static boolean saveGraphTask(@NonNull File databasePath, @NonNull DatabaseModel databaseModel) {
+        boolean result = false;
+        // return if graph disabled in settings or the database has not enough data
+        Cursor cursor = databaseModel.getCursor();
+        if (cursor != null && !cursor.isClosed()) {
+            if (cursor.getCount() > 1) { // check if there is enough data
+                cursor.moveToLast();
+                long endTime = cursor.getLong(cursor.getColumnIndex(DatabaseContract.TABLE_COLUMN_TIME));
+                cursor.close();
+                String outputFileDir = String.format(
+                        Locale.getDefault(),
+                        "%s/%s",
+                        DATABASE_HISTORY_PATH,
+                        DateFormat.getDateInstance(SHORT)
+                                .format(endTime)
+                                .replace("/", "_")
+                );
+                // rename the file if it already exists
+                File outputFile = new File(outputFileDir);
+                String baseFileDir = outputFileDir;
+                for (byte i = 1; outputFile.exists() && i < 127; i++) {
+                    outputFileDir = baseFileDir + " (" + i + ")";
+                    outputFile = new File(outputFileDir);
+                }
+                try {
+                    File directory = new File(DATABASE_HISTORY_PATH);
+                    if (!directory.exists()) {
+                        if (!directory.mkdirs()) {
+                            return false;
+                        }
+                    }
+                    FileInputStream inputStream = new FileInputStream(databasePath);
+                    FileOutputStream outputStream = new FileOutputStream(outputFile, false);
+                    byte[] buffer = new byte[1024];
+                    while (inputStream.read(buffer) != -1) {
+                        outputStream.write(buffer);
+                    }
+                    outputStream.flush();
+                    outputStream.close();
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                // TODO: shortenGraph(outputFile);
+                result = true;
+            }
+            cursor.close();
+        }
+        return result;
+    }
+
     public interface LineGraphSeriesReceiver {
         void generatingFinished(@Nullable LineGraphSeries<DataPoint>[] graphs);
     }
@@ -171,25 +246,7 @@ public final class DatabaseUtils {
 
         @Override
         protected LineGraphSeries<DataPoint>[] doInBackground(Void... voids) {
-            if (databaseValues[0] == null) {
-                return null;
-            }
-            LineGraphSeries<DataPoint>[] graphs = new LineGraphSeries[NUMBER_OF_GRAPHS];
-            graphs[GRAPH_INDEX_BATTERY_LEVEL] = new LineGraphSeries();
-            graphs[GRAPH_INDEX_TEMPERATURE] = new LineGraphSeries();
-            if (databaseValues[0].getVoltage() != 0)
-                graphs[GRAPH_INDEX_VOLTAGE] = new LineGraphSeries();
-            if (databaseValues[0].getCurrent() != 0)
-                graphs[GRAPH_INDEX_CURRENT] = new LineGraphSeries();
-            for (DatabaseValue value : databaseValues) {
-                DataPoint[] dataPoints = value.toDataPoints(useFahrenheit, reverseCurrent);
-                for (int i = 0; i < NUMBER_OF_GRAPHS; i++) {
-                    if (graphs[i] != null && dataPoints[i] != null) {
-                        graphs[i].appendData(dataPoints[i], false, databaseValues.length);
-                    }
-                }
-            }
-            return graphs;
+            return generateGraphsTask(databaseValues, useFahrenheit, reverseCurrent);
         }
 
         @Override
@@ -213,56 +270,7 @@ public final class DatabaseUtils {
 
         @Override
         protected Boolean doInBackground(Void... voids) {
-            boolean result = false;
-            // return if graph disabled in settings or the database has not enough data
-            Cursor cursor = databaseModel.getCursor();
-            if (cursor != null && !cursor.isClosed()) {
-                if (cursor.getCount() > 1) { // check if there is enough data
-                    cursor.moveToLast();
-                    long endTime = cursor.getLong(cursor.getColumnIndex(DatabaseContract.TABLE_COLUMN_TIME));
-                    cursor.close();
-                    String outputFileDir = String.format(
-                            Locale.getDefault(),
-                            "%s/%s",
-                            DATABASE_HISTORY_PATH,
-                            DateFormat.getDateInstance(SHORT)
-                                    .format(endTime)
-                                    .replace("/", "_")
-                    );
-                    // rename the file if it already exists
-                    File outputFile = new File(outputFileDir);
-                    String baseFileDir = outputFileDir;
-                    for (byte i = 1; outputFile.exists() && i < 127; i++) {
-                        outputFileDir = baseFileDir + " (" + i + ")";
-                        outputFile = new File(outputFileDir);
-                    }
-                    File inputFile = databasePath;
-                    try {
-                        File directory = new File(DATABASE_HISTORY_PATH);
-                        if (!directory.exists()) {
-                            if (!directory.mkdirs()) {
-                                return false;
-                            }
-                        }
-                        FileInputStream inputStream = new FileInputStream(inputFile);
-                        FileOutputStream outputStream = new FileOutputStream(outputFile, false);
-                        byte[] buffer = new byte[1024];
-                        while (inputStream.read(buffer) != -1) {
-                            outputStream.write(buffer);
-                        }
-                        outputStream.flush();
-                        outputStream.close();
-                        inputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return false;
-                    }
-                    // TODO: shortenGraph(outputFile);
-                    result = true;
-                }
-                cursor.close();
-            }
-            return result;
+            return saveGraphTask(databasePath, databaseModel);
         }
 
         @Override
