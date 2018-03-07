91fd705c14777172ab25fa61fa7c0595fa841b17
pixelfehler1996 <checkercraft1996@gmail.com>
2018-03-07 17:24:24 +0100

    fixed wrong temperature

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/services/BackgroundService.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/services/BackgroundService.java
@@ -927,16 +927,16 @@ public class BackgroundService extends Service {
             int temperature = intent.getIntExtra(EXTRA_TEMPERATURE, 0);
             // high battery temperature warning
             if (tempHighWarningEnabled && !tempHighNotified) {
-                int warningTemperature = sharedPreferences.getInt(getString(R.string.pref_temp_high_warning), getResources().getInteger(R.integer.pref_temp_high_warning_default)) * 10;
-                if (temperature >= warningTemperature) {
+                int warningTemperature = sharedPreferences.getInt(getString(R.string.pref_temp_high_warning), getResources().getInteger(R.integer.pref_temp_high_warning_default));
+                if (temperature >= warningTemperature * 10) {
                     tempHighNotified = true;
                     showTemperatureWarning(true, warningTemperature);
                 }
             }
             // low battery temperature warning
             if (tempLowWarningEnabled && !tempLowNotified) {
-                int warningTemperature = sharedPreferences.getInt(getString(R.string.pref_temp_low_warning), getResources().getInteger(R.integer.pref_temp_low_warning_default)) * 10;
-                if (temperature <= warningTemperature) {
+                int warningTemperature = sharedPreferences.getInt(getString(R.string.pref_temp_low_warning), getResources().getInteger(R.integer.pref_temp_low_warning_default));
+                if (temperature <= warningTemperature * 10) {
                     tempLowNotified = true;
                     showTemperatureWarning(false, warningTemperature);
                 }
