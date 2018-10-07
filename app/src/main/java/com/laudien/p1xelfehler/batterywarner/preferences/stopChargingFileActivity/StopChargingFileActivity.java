8908dd26845e14333adb41b91b684002c6fc3761
P1xelfehler <norman.laudien1996@gmail.com>
2018-10-07 15:09:23 +0200

    added "send file to developer" menu action

--- app/src/main/java/com/laudien/p1xelfehler/batterywarner/preferences/stopChargingFileActivity/StopChargingFileActivity.java
+++ app/src/main/java/com/laudien/p1xelfehler/batterywarner/preferences/stopChargingFileActivity/StopChargingFileActivity.java
@@ -1,13 +1,23 @@
 package com.laudien.p1xelfehler.batterywarner.preferences.stopChargingFileActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.laudien.p1xelfehler.batterywarner.BaseActivity;
-import com.laudien.p1xelfehler.batterywarner.MainActivity;
 import com.laudien.p1xelfehler.batterywarner.R;
+import com.laudien.p1xelfehler.batterywarner.helper.ToastHelper;
+
+import static android.os.Build.VERSION.SDK_INT;
 
 public class StopChargingFileActivity extends BaseActivity {
     @Override
@@ -20,28 +30,55 @@ public class StopChargingFileActivity extends BaseActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        if (isTaskRoot()) {
-            backStackBack();
-        }
-        super.onBackPressed();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.stop_charging_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home && isTaskRoot()) {
-            backStackBack();
-            finish();
-            return true;
+        if (item.getItemId() == R.id.menu_sent_file_to_developer) {
+            sendFileToDeveloper();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * This method will be executed if you click back in navigation keys or toolbar.
-     * It starts the main activity.
-     */
-    private void backStackBack() {
-        startActivity(new Intent(this, MainActivity.class));
+    private void sendFileToDeveloper() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.developer_mail)});
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name));
+        intent.putExtra(Intent.EXTRA_TEXT,
+                "\n------------------" +
+                        "\nDebug information" +
+                        "\n------------------" +
+                        "\nApp version: " + getAppVersion() +
+                        "\nAndroid version: " + SDK_INT +
+                        "\nDevice: " + Build.DEVICE +
+                        "\nBrand: " + Build.BRAND +
+                        "\nModel: " + Build.MODEL +
+                        "\nProduct: " + Build.PRODUCT +
+                        "\nFile path: " + prefs.getString(getString(R.string.pref_stop_charging_file), "") +
+                        "\nchargeOn: " + prefs.getString(getString(R.string.pref_stop_charging_enable_charging_text), getString(R.string.pref_stop_charging_enable_charging_text_default)) +
+                        "\nchargeOff: " + prefs.getString(getString(R.string.pref_stop_charging_disable_charging_text), getString(R.string.pref_stop_charging_disable_charging_text_default))
+        );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            ToastHelper.sendToast(this, R.string.toast_no_email_app);
+        }
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Nullable
+    private String getAppVersion() {
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            return String.format("%s (%d)", pInfo.versionName, pInfo.versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
